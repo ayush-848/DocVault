0200381b9b3c4d02701617f5fe8f7010ba4e057e
Ayush Deb <ayushdeb848@gmail.com>
2025-07-01 23:23:04 +0530

    increased jwt token age

--- server/utils/generateToken.js
+++ server/utils/generateToken.js
@@ -4,7 +4,7 @@ const generateToken = (user) => {
   return jwt.sign(
     { id: user.id, username: user.username },
     process.env.JWT_SECRET,
-    { expiresIn: '1h' }
+    { expiresIn: '24h' }
   );
 };
 
